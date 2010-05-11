e3871e369572f79028fe8781fa9487242341e783
Steve Pfisterer <stevepfisterer@gmail.com>
2010-05-10 20:07:27 -0500

    update initial environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -21,7 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "image_science"
-  config.gem "authlogic"  
+  config.gem "authlogic", :version => '< 2'
   config.gem "crack"
   config.gem "httparty"
   config.gem "matthooks-vimeo", :lib => "vimeo"
