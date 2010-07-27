4aa02d4fc7bb4427fc8451f918943028eb5871e4
Steve Pfisterer <stevepfisterer@gmail.com>
2010-07-27 17:46:51 -0500

    take out commentor role if feature is not enabled, don't let blue admins be edited by non blue admins

--- app/helpers/admin/admin_users_helper.rb
+++ app/helpers/admin/admin_users_helper.rb
@@ -4,7 +4,9 @@ module Admin::AdminUsersHelper
     conditions = []
     conditions = ["name != ?", "Blue Admin"]  unless current_admin_user.has_permission?(:blue_admin)
     
-    @roles ||= AdminUserRole.find(:all, :conditions => conditions, :order => :name)
+    @roles ||= AdminUserRole.find(:all, :conditions => conditions, :order => :name).delete_if do |role|
+      role.name == "Commentator" && Span::Blue.features.include?(:comments) == false
+    end
   end
   
 end
