4230f25a1469d44da610a6ed62606ba080f81c77
Steve Pfisterer <stevepfisterer@gmail.com>
2010-07-27 16:55:26 -0500

    navigation options toggle via Blue Features

--- config/initializers/blue.rb
+++ config/initializers/blue.rb
@@ -1,4 +1,6 @@
 # modified by blue
 # Page.types = [PageTypes::SubPage, PageTypes::HomePage, PageTypes::Link ]
 # Span::Blue.features << :membership
+# Span::Blue.features << :comments
+# Span::Blue.features << :collapsible_navigation
 # Page.allow_ssl = true
