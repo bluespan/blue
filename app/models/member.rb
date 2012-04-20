632105619d95b56351e6a00ad0f49c5d044c43d1
Steve Pfisterer <stevepfisterer@gmail.com>
2012-04-20 11:04:14 -0500

    added name method to member

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,4 +2,9 @@ class Member < ActiveRecord::Base
   acts_as_authentic :session_class => "MemberSession"
   
   has_assigned_member_roles
+  
+  def name
+    "#{firstname} #{lastname}"
+  end
+  
 end
