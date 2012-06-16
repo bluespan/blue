2a2bdd449335783347869d293840ac3f96a8c98a
Steve Pfisterer <stevepfisterer@gmail.com>
2012-06-15 20:16:25 -0500

    only rewrite thumbnail url in verbiage editor for image's src attribute

--- assets/javascripts/admin/verbiage.js
+++ assets/javascripts/admin/verbiage.js
@@ -4,8 +4,7 @@ $(document).ready(function(){
 	bind_verbiage_field_dialog(".edit_verbiage_field")
 });
 
-
-var wymeditor_config = {};
+if (wymeditor_config == null) var wymeditor_config = {};
 var verbiage_updated_callback = function() {};
 
 // Page Dialogs
@@ -43,7 +42,7 @@ var verbiage_dialog = {
   						//html = html.replace(/src=&quot;\/assets\/images\/([^\.])/gi, 'src=&quot;/assets/images/.thumbnails/$1');
   						//html = html.replace(/src=&quot;(.*)\/assets\/images\/([^\.].*)&quot;/gi, 'src=&quot;$1/assets/images/.thumbnails/$2&quot;');
   						//html = html.replace(/src="(.*)\/assets\/images\/([^\.].*)"/gi, 'src="$1/assets/images/.thumbnails/$2"');
-  				    html = html.replace(/assets\/images/gi, "assets/images/.thumbnails").replace(/\.thumbnails\/\.thumbnails/gi, "/.thumbnails")
+  				    html = html.replace(/src=(.*)\/assets\/images/gi, "src=$1/assets/images/.thumbnails").replace(/\.thumbnails\/\.thumbnails/gi, "/.thumbnails")
   						dialog.data.html('<div class="content">' + html + '</div>');
 
   						dialog.data.find('.close').click(function () { $.modal.close(); return false; });
